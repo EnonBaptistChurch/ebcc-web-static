621f13c749168495239ccac054e67e240cf854e5
David <davidmitchellfootball@googlemail.com>
2025-07-10 18:39:38 +0100

    refactor CalendarEventComponent to improve date and time formatting

--- components/events/CalendarEventComponent.vue
+++ components/events/CalendarEventComponent.vue
@@ -2,17 +2,11 @@
   <div class="event-box">
     <h3 class="event-title">{{ event.summary }}</h3>
     <div class="event-time-display">
-      <p class="italic-text"> {{getDate(event.start.dateTime || event.start.date) +  " (" + getDay(event.start.dateTime || event.start.date)  + ")"}} </p>
-      
+      <p class="italic-text date"> {{ getDateDayMonth(event.start.dateTime || event.start.date) }} </p>
+      <p class="event-time time">{{ getTime(event.start.dateTime || event.start.date) }} - {{ getTime(event.end.dateTime || event.end.date) }}</p>
     </div>
-    <p class="event-time">{{ getTime(event.start.dateTime || event.start.date) }} - {{ getTime(event.end.dateTime || event.end.date) }}</p>
     <p class="event-description" v-if="event.description">{{ event.description }}</p>
-    <div >
-      <p class="event-recurring" v-if="event.isRecurring"></p>
-      <!-- <p class="more-info">More Information</p> -->
-       <!-- <div style="display: flex-end;"><DirectionsComponent :location="event.location" :text="text"  /></div> -->
     
-  </div>
   </div>
 </template>
 
@@ -21,17 +15,25 @@ import type { CalendarEvent } from '@/types/CalendarEvent';
 
 const props = defineProps<{ event: CalendarEvent }>();
 const text: string = "";
-// Function to format day of the week
-function getDay(dateString?: string): string {
+
+function getDateDayMonth(dateString?: string): string {
   if (!dateString) return 'Unknown date';
   const date = new Date(dateString);
-  return date.toLocaleString('en-GB', { weekday: 'long' });
+  const dayOfWeekString = date.toLocaleString('en-GB', { weekday: 'short' })
+  const dayNum: number = date.getDate();
+  const suffix: string = getOrdinalSuffix(dayNum);
+  const month = date.toLocaleString('default', { month: 'long' });
+  return `${dayOfWeekString} ${dayNum}${suffix} ${month}`;
 }
 
-function getDate(dateString?: string): string {
-  if (!dateString) return 'Unknown date';
-  const date = new Date(dateString);
-  return date.toLocaleDateString('en-GB');
+function getOrdinalSuffix(day: number) {
+  if (day >= 11 && day <= 13) return 'th'; 
+  switch (day % 10) {
+    case 1: return 'st';
+    case 2: return 'nd';
+    case 3: return 'rd';
+    default: return 'th';
+  }
 }
 
 function getTime(dateString?: string): string {
@@ -44,12 +46,18 @@ function getTime(dateString?: string): string {
 
 <style scoped>
 .event-box {
-  padding: 10px;
+  padding: 5px 10px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   width: 275px;
   border-radius: 18px !important;
 }
+.date {
+  justify-content: flex-start;
+}
 
+.time {
+  justify-content: flex-end;
+}
 .event-box a {
   border-radius: 18px
 }
@@ -77,8 +85,10 @@ function getTime(dateString?: string): string {
 .event-time-display {
   display: flex;
   color: initial;
-  margin: 0.25rem 0rem;
+  margin: 0rem;
   font-size: 1em;
+  justify-content: space-between;
+  width: 100%;
 
 }
 
