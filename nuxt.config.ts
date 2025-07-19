6d40c8f2a74d9db6a730ca9baeb3c37a3f659bbd
David <30800534+david-p-mitchell@users.noreply.github.com>
2025-07-20 00:38:52 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import { fetchCalendarEvents } from './utils/fetchCalendar'
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
-  modules: ['@nuxt/image-edge', 'node-ical'],
+  modules: ['@nuxt/image-edge'],
   css: ['~/assets/css/site.css'], // Adjust to match your actual path
   alias: {
     '@': './',
