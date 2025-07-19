074b150b61ef0cbaef70e83dea638ec5097753af
David <davidmitchellfootball@googlemail.com>
2025-07-19 22:58:20 +0100

    fix with text-content and added content

--- pages/whats-on/online-prayer-meeting.vue
+++ pages/whats-on/online-prayer-meeting.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <p> PlaceHolder Text</p>
+    <div class="text-content">
+        <p>For those who can not normally get to the weekly prayer meeting, we have an online prayer meeting, usually around once a month.</p>
         <a href= "https://us02web.zoom.us/j/84104984721?pwd=UG5wNGp2eXpDUWtUVmZoRFBPcjVvUT09">Click Here for Zoom Link</a>
     </div>
 </template>
