34f35e4d5498ebf4d6a76cae4b9541b439b7ff5b
David <davidmitchellfootball@googlemail.com>
2025-07-19 22:52:07 +0100

    settle calendar for now

--- components/events/CalendarEventsComponent.vue
+++ components/events/CalendarEventsComponent.vue
@@ -1,4 +1,5 @@
 <template>
+  <div class="event-wrapper">
   <h2 class="event-underline">Upcoming Events</h2>
 
   <div class="event-container">
@@ -12,20 +13,23 @@
     </template>
     <!-- Show actual events when loaded -->
     <template v-else>
-      <div
-        v-for="event in events"
-        :key="event.id"
-        class="event-box-container"
-      >
-        <div v-if="event.summary === 'Sunday Club'"> <SundayClubEventComponent :event="event" /> </div>
-        <div v-else-if="event.summary === 'Morning Service'"> <SundayMorningComponent :event="event" /> </div>
-        <div v-else-if="event.summary === 'Prayer Meeting'"> <SundayPrayerEventComponent :event="event" /> </div>
-        <div v-else-if="event.summary === 'Evening Service'"> <SundayEveningComponent :event="event" /> </div>
-        <div v-else-if="event.summary === 'Bible Study & Prayer Meeting'"> <BibleStudyEventComponent :event="event" /> </div>
-        <div v-else-if="event.summary === 'Discoverers'"> <DiscoverersEventComponent :event="event" /> </div>
-        <div v-else class="event-box"> <CalendarEventComponent :event="event" /> </div>
+      <div class="loading-grid">
+        <div
+          v-for="event in events"
+          :key="event.id"
+          class="event-box-container"
+        >
+          <div v-if="event.summary === 'Sunday Club'"> <SundayClubEventComponent :event="event" /> </div>
+          <div v-else-if="event.summary === 'Morning Service'"> <SundayMorningComponent :event="event" /> </div>
+          <div v-else-if="event.summary === 'Prayer Meeting'"> <SundayPrayerEventComponent :event="event" /> </div>
+          <div v-else-if="event.summary === 'Evening Service'"> <SundayEveningComponent :event="event" /> </div>
+          <div v-else-if="event.summary === 'Bible Study & Prayer Meeting'"> <BibleStudyEventComponent :event="event" /> </div>
+          <div v-else-if="event.summary === 'Discoverers'"> <DiscoverersEventComponent :event="event" /> </div>
+          <div v-else > <CalendarEventComponent :event="event" :has-page="false" /> </div>
+        </div>
       </div>
     </template>
+  </div>
   </div>
 </template>
 
@@ -73,11 +77,13 @@ onMounted(async () => {
 
 .event-underline {
   border-bottom: 1px solid var(--nav-bar-bg-color);
+  max-width: 1250px;
+  width:95%;
 }
 
 @media (min-width: 1024px) {
   .event-container {
-    height: 128px;
+    min-height: 128px;
   }
 }
 
@@ -87,5 +93,45 @@ onMounted(async () => {
   height: 120px;       /* Adjust height similarly */
   background-color: #ccc;
   border-radius: 6px;
+  display:block;
+}
+
+
+.event-box {
+  padding: 5px 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  width: 275px;
+  border-radius: 18px !important;
+}
+
+.event-box a {
+  border-radius: 18px
+}
+
+.event-box:hover {
+  cursor: pointer;
+}
+
+.loading-grid {
+  display: grid;
+  gap: 1rem;
+  justify-content: center;
+  grid-template-columns: repeat(4, 315px);
+}
+
+@media (max-width: 1300px) {
+  .loading-grid {
+    grid-template-columns: repeat(2, 315px);
+  }
+}
+
+@media (max-width: 600px) {
+  .loading-grid {
+    grid-template-columns: 100%;
+  }
+  .event-box {
+    width: 100%;
+    min-width: 100%;
+  }
 }
 </style>
