fc07f7ed7e910daa870f8261b079bccdc4d71563
David <davidmitchellfootball@googlemail.com>
2025-07-19 22:57:19 +0100

    adding Sunday morning and evening componentts

---
+++ components/sundays/SundayMorning.vue
@@ -0,0 +1,9 @@
+<template>
+<div class="text-content">
+    <h1>Sunday Morning Service - 11:15AM</h1>
+    <p>Join us for our Sunday morning service, where we gather to worship, pray, and hear from God's Word.</p>
+    
+    <p>Parents with little ones are welcome too! There’s a crèche available in the room just behind the pastor, where you can take your children if needed.</p>
+    <p>For older children, we provide sermon worksheets partway through the service to help them engage with what’s being taught.</p>
+</div>
+</template>
