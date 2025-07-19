4b9f5b54921634c2a649ffc40cf13f2060637583
David <davidmitchellfootball@googlemail.com>
2025-07-19 20:10:41 +0100

    move date and time methods out

--- components/events/CalendarEventComponent.vue
+++ components/events/CalendarEventComponent.vue
@@ -12,36 +12,8 @@
 
 <script setup lang="ts">
 import type { CalendarEvent } from '@/types/CalendarEvent';
-
+import { getDateDayMonth, getTime } from '@/composables/useDateToText';
 const props = defineProps<{ event: CalendarEvent }>();
-const text: string = "";
-
-function getDateDayMonth(dateString?: string): string {
-  if (!dateString) return 'Unknown date';
-  const date = new Date(dateString);
-  const dayOfWeekString = date.toLocaleString('en-GB', { weekday: 'short' })
-  const dayNum: number = date.getDate();
-  const suffix: string = getOrdinalSuffix(dayNum);
-  const month = date.toLocaleString('default', { month: 'long' });
-  return `${dayOfWeekString} ${dayNum}${suffix} ${month}`;
-}
-
-function getOrdinalSuffix(day: number) {
-  if (day >= 11 && day <= 13) return 'th'; 
-  switch (day % 10) {
-    case 1: return 'st';
-    case 2: return 'nd';
-    case 3: return 'rd';
-    default: return 'th';
-  }
-}
-
-function getTime(dateString?: string): string {
-  if (!dateString) return 'Unknown Time';
-  const date = new Date(dateString);
-  return date.toLocaleString('en-GB', { hour: 'numeric', minute: '2-digit' });
-}
-
 </script>
 
 <style scoped>
