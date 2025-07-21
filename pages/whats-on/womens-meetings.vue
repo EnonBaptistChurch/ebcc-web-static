04911efc9a192917356285a7e1145f8ea8d15194
David <davidmitchellfootball@googlemail.com>
2025-07-21 16:47:51 +0100

    correction on Ladies Meeting

--- pages/whats-on/womens-meetings.vue
+++ pages/whats-on/womens-meetings.vue
@@ -12,6 +12,6 @@ export default {
     <div class="text-content">
         <h1>Ladies Meetings</h1>
         <p>Ladies Meeting – 1st and 3rd Thursday each month at 1.45pm </p>
-        <p>Monthly Bible Study on Wednesdays – please <CallWidget text = "ring"/> for details  </p>
+        <p>Monthly Bible Study on Tuesdays – please contact for details  </p>
     </div>
 </template>
