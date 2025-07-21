0e6cf35af8bca0b2b61e60101d621500196d80c5
David <davidmitchellfootball@googlemail.com>
2025-07-21 16:47:30 +0100

    sunday prayer content fill

--- pages/whats-on/sunday-prayer.vue
+++ pages/whats-on/sunday-prayer.vue
@@ -1,3 +1,9 @@
 <template>
-
+<div class="text-content">
+    <h1>Discoverers</h1>
+        <div>
+            <p>When it isn't a communion service, we have a Sunday prayer service at 6pm.</p>
+            <P>There will be a brief thought on the scriptures and then a short time of prayer together.</P>
+        </div>
+</div>
 </template>
