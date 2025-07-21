24eac00428f5dd3dd247b6a10e533da3654bc62c
David <davidmitchellfootball@googlemail.com>
2025-07-21 15:32:07 +0100

    add Maps Widget

---
+++ components/widgets/MapsWidget.vue
@@ -0,0 +1,11 @@
+<template>
+<div>
+    <div class="text-content">
+    <h2>Find Us</h2>
+        <p>We are located at: Enon Baptist Church, Skinner Street, Chatham, ME4 5RF, UK</p>
+    <div class ="mapcontainer">
+    </div>
+    </div>
+    <iframe style="border: 0;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2490.338154880195!2d0.5245410153408684!3d51.378462827943984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8cd053f801537%3A0x39e4cda4ed625544!2sEnon+Baptist+Church!5e0!3m2!1sen!2suk!4v1485452280704" width="100%" height="300px" frameborder="0" :allowfullscreen="true" class="maps-iframe" ></iframe>
+</div>
+</template>
