b86ad9bf34d87a2e3eb7ca27129062e53d5a9b39
David <davidmitchellfootball@googlemail.com>
2025-08-06 16:59:33 +0100

    rm dupe time

--- components/sundays/SundayEvening.vue
+++ components/sundays/SundayEvening.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="text-content">
     <h1>Sunday Evening Service - 6:30PM</h1>
-    <p>Join us for our Sunday evening service, where we gather to worship, pray, and hear from God's Word. The service starts at 6:30 PM.</p>
+    <p>Join us for our Sunday evening service, where we gather to worship, pray, and hear from God's Word.</p>
     <p>We encourage all who are able to attend to join us.</p>
     <p>For those unable to join us in person, the evening service is also accessible via Zoom <NuxtLink href="https://us02web.zoom.us/j/468947237?pwd=OGJaRDdqaFdabzZ2VEdacFdIS3ZMQT09"> here</NuxtLink>.</p>
     </div>
