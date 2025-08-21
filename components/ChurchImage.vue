7801e3f85900c8956b1c4d2a8589ae197ccf8801
David <davidmitchellfootball@googlemail.com>
2025-08-22 00:44:37 +0100

    trying different sizes

--- components/ChurchImage.vue
+++ components/ChurchImage.vue
@@ -7,7 +7,22 @@
   preload
   loading="eager"
 /> -->
-  <img fetchpriority="high" src="/images/enonChurch.webp" alt="Enon Baptist Church"  @load="notifyParent" class="responsive-image" width="1487" height="600" />
+  <!-- <img fetchpriority="high" src="/images/enonChurch.webp" alt="Enon Baptist Church"  @load="notifyParent" class="responsive-image" width="1487" height="600" /> -->
+   <img
+  src="/images/enonChurch-1920.webp"
+  srcset="
+    /images/enonChurch-320.webp 320w,
+    /images/enonChurch-480.webp 480w,
+    /images/enonChurch-640.webp 640w,
+    /images/enonChurch-768.webp 768w,
+    /images/enonChurch-1024.webp 1024w,
+    /images/enonChurch-1280.webp 1280w,
+    /images/enonChurch-1440.webp 1440w,
+    /images/enonChurch-1920.webp 1920w"
+  sizes="100vw"
+  alt="Enon Baptist Church"
+  class="responsive-image"
+/>
   
   
 </template>
