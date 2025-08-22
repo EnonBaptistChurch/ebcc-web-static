7dc097f02d9b61df0dfc3631d59ddfac114db160
David <davidmitchellfootball@googlemail.com>
2025-08-22 17:59:58 +0100

    for multiple site image sizes and using with src-sets

---
+++ components/SiteImage.vue
@@ -0,0 +1,24 @@
+<template>
+  <img
+    :src="src"
+    :srcset="srcset"
+    :sizes="sizes"
+    :alt="alt"
+    :class="cls"
+  />
+</template>
+
+<script setup>
+const props = defineProps({
+  src: String,
+  sizes: String,
+  alt: { type: String, default: '' },
+  cls: String
+});
+
+const widths = [320, 480, 640, 768, 1024, 1280, 1440, 1920];
+
+const srcset = widths
+  .map(w => `${props.src}?w=${w} ${w}w`)
+  .join(', ')
+</script>
