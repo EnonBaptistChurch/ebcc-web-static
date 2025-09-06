900d3156118a42bdece1edcc1eb523dbdc94ff6f
David <davidmitchellfootball@googlemail.com>
2025-09-07 00:22:41 +0100

    change meta description for Bible Study.

--- pages/whats-on/bible-study.vue
+++ pages/whats-on/bible-study.vue
@@ -4,9 +4,23 @@
         <h1>Bible Study</h1>
         <section>
             <p>
-            <strong>Wednesday at 7:30 pm</strong> – We value the Bible as God’s Word and meet each week to study it and learn from its teaching. We also spend time in prayer, seeking God’s help in all that we do as a church and praying for the advance of the Gospel (the good news about Jesus) around the world.
+            <strong>Wednesday at 7:30 pm</strong> – We value the Bible as God’s Word and meet each week to study it and learn from its teaching. </p> 
+            <p> We also spend time in prayer, seeking God’s help in all that we do as a church and praying for the advance of the Gospel (the good news about Jesus) around the world.
             </p>
         </section>
     </div>
     </div>
 </template>
+
+<script setup>
+useHead({
+  title: 'Bible Study | Enon Baptist Church',
+  meta: [
+    {
+      name: 'description',
+      content:
+        'Join us for Bible Study at Enon to explore and study God’s Word together, and engage in prayer for our church and the whole world.',
+    }
+  ]
+})
+</script>
